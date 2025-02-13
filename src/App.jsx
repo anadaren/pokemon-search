57fcc8a19fe0b4faf7e4bf6a987126474ac785d6
Ana Green <greena22@tcnj.edu>
2025-02-13 18:24:59 -0500

    New features

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,158 @@
-import { useState } from 'react'
+import { useState,useRef } from 'react'
 import './App.css'
 import '/src/fonts/PokemonGB.ttf'
 
-async function fetchData(){
-	try{
-    const searchedPokemon = document.getElementById('searchedPokemon').value.toLowerCase();
-		const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchedPokemon}`);
-    if(!response.ok){
-			throw new Error('Could not fetch resource.');
-		}
-		const data = await response.json();
-    console.log(data);
-    // Get Name
-    const pokemonName = data.name;
-    const nameElement = document.getElementById('pokemonName');
-    nameElement.innerText = pokemonName;
-    // Get Number
-    const pokemonNumber = data.order;
-    const numElement = document.getElementById('pokemonNumber');
-    numElement.innerText = `No.` + pokemonNumber;
-		// Get Sprite
-    const pokemonSprite = data.sprites.front_default;
-    const imgElement = document.getElementById('pokemonSprite');
-    imgElement.src = pokemonSprite;
-    imgElement.style.display = "block";
-    // Get Type(s)
-    const pokemonType = data.types;
-    const typeElement = document.getElementById('pokemonType');
-    typeElement.innerText = `Type: `;
-    let typ = 0;
-    for (const i of data.types) {
-      if(typ>0) typeElement.innerText += ',';
-      typeElement.innerText += i.type.name;
-      typ++;
-    }
-    
-	}
-	catch(error){
-		console.error(error);
-	}
-}
 
 function App() {
+  const [searchedPokemon, setSearchedPokemon] = useState("");
+  const [pokemonNumber, setPokemonNumber] = useState();
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonSprite, setPokemonSprite] = useState("");
+  const [pokemonSprite2, setPokemonSprite2] = useState("");
+  const [pokemonSprite3, setPokemonSprite3] = useState("");
+  const [pokemonType, setPokemonType] = useState("");
+  const [pokemonCry, setPokemonCry] = useState(null);
+  const [pokemonStats, setPokemonStats] = useState([]);
+  const [pokemonMoves, setPokemonMoves] = useState([]);
+  const [screenIndex, setScreenIndex] = useState(0);
+
+  const audioRef = useRef(null);
+  const clickSoundRef = useRef(new Audio('/button.mp3'));
+
+  async function fetchData(searchedPokemon){
+    try{
+      clickSoundRef.current.play();
+
+      // Fetch Pokeapi API data
+      if(typeof searchedPokemon === 'string') {searchedPokemon = searchedPokemon.toLowerCase() }
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchedPokemon}`);
+      if(!response.ok){
+        throw new Error('Could not fetch resource.');
+      }
+      const data = await response.json();
+
+      // Get Name
+      setPokemonName(data.name);
+      // Get Number
+      setPokemonNumber(data.id);
+      // Get Front Sprite
+      setPokemonSprite(data.sprites.front_default);
+      // Get Back Sprite
+      setPokemonSprite2(data.sprites.back_default);
+       // Get Shiny Sprite
+       setPokemonSprite3(data.sprites.front_shiny);
+      // Get Type(s)
+      setPokemonType(data.types.map(t => t.type.name).join(","));
+      // Get Cry
+      setPokemonCry(data.cries.latest);
+      // Get Stats
+      setPokemonStats(data.stats);
+      // Get Moves
+      setPokemonMoves(data.moves);
+  
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
+  
+   // Pokemon click sound effect
+   const ButtonClick = () => {
+    clickSoundRef.current.play();
+  };
+
+  // Black button plays pokemon's cry
+  const PlayCry = () => {
+    if(audioRef.current) { audioRef.current.play(); }
+  }
+
+  // Red and Blue buttons toggle between views
+  const toggleScreen = (direction) => {
+    ButtonClick();
+    setScreenIndex((prevIndex) => (prevIndex + direction + 3) % 3);
+  };
+
+  // Render what's on the pokedex screen
+  const renderScreenContent = () => {
+    switch (screenIndex) {
+      case 0:
+        return (
+          <>
+              {pokemonName && <p>{pokemonName}</p>}
+              {pokemonNumber && <p>No. {pokemonNumber}</p>}
+              {pokemonSprite && <img src={pokemonSprite} alt="Pokemon Sprite" />}
+              {pokemonType && <p id="type">Type: {pokemonType}</p>}
+          </>
+        );
+      case 1:
+        return (
+          <>
+            {pokemonSprite2 && <img src={pokemonSprite2} alt="Pokemon Sprite" />}
+            {pokemonName && <p>Base Stats: </p>}
+            {pokemonStats ? (
+              pokemonStats.map((stat, index) => (
+                <p key={index}>
+                  {stat.stat.name}: {stat.base_stat}
+                </p>
+              ))
+            ) : (
+              <p>Loading stats...</p>
+            )}
+          </>
+          );
+      case 2:
+        return (
+          <>
+            {pokemonName && <p>Shiny Form: </p>}
+            {pokemonSprite3 && <img src={pokemonSprite3} alt="Pokemon Sprite" />}
+            {pokemonName && <p>Moveset: </p>}
+
+            {pokemonMoves ? (
+              pokemonMoves.map((move, index) => (
+                <p id='moves' key={index}>
+                  {move.move.name}
+                </p>
+              ))
+            ) : (
+              <p>Loading moves...</p>
+            )}
+          </>
+          );
+      default:
+        return null;
+    }
+  };
+
+  /*window.addEventListener('mousemove', handleMouseMove);
+const handleMouseMove = (e) => {
+  let tooltip = document.getElementsByClassName("tooltiptext");
+  var x = e.clientX,
+      y = e.clientY;
+  tooltip.style.top = (y + 20) + 'px';
+  tooltip.style.left = (x + 20) + 'px';
+};*/
+
 
   return (
     <>
+    {pokemonCry && <audio ref={audioRef} src={pokemonCry} />}
     <div className="main-container">
     <div className="input-container">
       <h1>Pokemon Search</h1>
-      <input type="text" id="searchedPokemon"
-        placeholder='Enter Pokemon' 
-        onKeyPress={event => {
-            if(event.key == "Enter") {
-              fetchData();
+      <input
+        type="text"
+        id="searchedPokemon"
+        placeholder='Name or No.' 
+        value={searchedPokemon}
+        onChange={(e) => setSearchedPokemon(e.target.value)}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              fetchData(searchedPokemon);
             }
           }}
         />
-      <button id="search" onClick={fetchData}>Search Pokemon</button>
+
+      <button id="search" onClick={() => fetchData(searchedPokemon)}>Search Pokemon</button>
       </div>
       <div className="dex-container">
         <div className="red-bg">
@@ -79,12 +175,8 @@ function App() {
               <div className="red-circle"></div>
             </div>
             <div className="screen">
-              <p id="pokemonNumber"></p>
-              <p id="pokemonName"></p>
-              <br></br>
-              <img src="" alt="Pokemon Sprite" id="pokemonSprite" ></img>
-              <br></br>
-              <p id="pokemonType"></p>
+              {renderScreenContent()}
+              
             </div>
 
             <div className="bottom-circles">
@@ -98,15 +190,25 @@ function App() {
           </div>
 
           <div className="buttons-container">
-            <div className="black-button"></div>
+            <div className="black-button" onClick={PlayCry}><span className="tooltiptext">Cry</span></div>
             <div className="color-buttons">
               <div className="small-buttons">
-                <div className="red-button"></div>
-                <div className="blue-button"></div>
+                <div className="red-button" onClick={() => toggleScreen(-1)}><span className="tooltiptext">Prev</span></div>
+                <div className="blue-button" onClick={() => toggleScreen(1)}><span className="tooltiptext">Next</span></div>
               </div>
               <div className="green-section"></div>
             </div>
-            <div className="d-pad"></div>
+            <div className="d-pad">
+              <div className='vertical'>
+                <div className='up'></div>
+                <div className='center'></div>
+                <div className='down'></div>
+              </div>
+              <div className='horizontal'>
+                <div className='left' onClick={() => {ButtonClick(); fetchData(pokemonNumber - 1);}}><span className="tooltiptext">Prev Pokemon</span></div>
+                <div className='right' onClick={() => {ButtonClick(); fetchData(pokemonNumber + 1);}}><span className="tooltiptext">Next Pokemon</span></div>
+              </div>
+            </div>
           </div>
 
         </div>
@@ -117,8 +219,7 @@ function App() {
         </div>
       </div>
       </div>
-
-
+      
       <p className="footer">
         Check out my <a href="https://github.com/anadaren">Github</a>
       </p>
