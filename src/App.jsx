ab285982d2ddbea0342e23fa7c457b7d1748604d
Ana Green <greena22@tcnj.edu>
2025-02-13 22:50:23 -0500

    Tooltip fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useRef } from 'react'
+import { useEffect,useState,useRef } from 'react'
 import './App.css'
 import '/src/fonts/PokemonGB.ttf'
 
@@ -15,6 +15,16 @@ function App() {
   const [pokemonStats, setPokemonStats] = useState([]);
   const [pokemonMoves, setPokemonMoves] = useState([]);
   const [screenIndex, setScreenIndex] = useState(0);
+
+  
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [tooltipCryVisible, setTooltipCryVisible] = useState(false);
+  const [tooltipPrevVisible, setTooltipPrevVisible] = useState(false);
+  const [tooltipNextVisible, setTooltipNextVisible] = useState(false);
+  const [tooltipLeftVisible, setTooltipLeftVisible] = useState(false);
+  const [tooltipRightVisible, setTooltipRightVisible] = useState(false);
+  
 
   const audioRef = useRef(null);
   const clickSoundRef = useRef(new Audio('/button.mp3'));
@@ -87,11 +97,12 @@ function App() {
       case 1:
         return (
           <>
+            {pokemonName && <p>Back: </p>}
             {pokemonSprite2 && <img src={pokemonSprite2} alt="Pokemon Sprite" />}
             {pokemonName && <p>Base Stats: </p>}
             {pokemonStats ? (
               pokemonStats.map((stat, index) => (
-                <p key={index}>
+                <p id='stats' key={index}>
                   {stat.stat.name}: {stat.base_stat}
                 </p>
               ))
@@ -123,14 +134,86 @@ function App() {
     }
   };
 
-  /*window.addEventListener('mousemove', handleMouseMove);
-const handleMouseMove = (e) => {
-  let tooltip = document.getElementsByClassName("tooltiptext");
-  var x = e.clientX,
-      y = e.clientY;
-  tooltip.style.top = (y + 20) + 'px';
-  tooltip.style.left = (x + 20) + 'px';
-};*/
+  // Tooltips
+  useEffect(() => {
+    const handlePointerMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    const handleMouseEnter = (tooltipType) => {
+      switch (tooltipType) {
+        case 'cry': setTooltipCryVisible(true); break;
+        case 'next': setTooltipNextVisible(true); break;
+        case 'prev': setTooltipPrevVisible(true); break;
+        case 'left': setTooltipLeftVisible(true); break;
+        case 'right': setTooltipRightVisible(true); break;
+      }
+    };
+    
+    const handleMouseLeave = (tooltipType) => {
+      switch (tooltipType) {
+        case 'cry': setTooltipCryVisible(false); break;
+        case 'next': setTooltipNextVisible(false); break;
+        case 'prev': setTooltipPrevVisible(false); break;
+        case 'left': setTooltipLeftVisible(false); break;
+        case 'right': setTooltipRightVisible(false); break;
+      }
+    };
+
+    document.body.addEventListener("pointermove", handlePointerMove);
+    const cryHover = document.getElementById("black-button");
+    const prevHover = document.getElementById("red-button");
+    const nextHover = document.getElementById("blue-button");
+    const leftHover = document.getElementById("left");
+    const rightHover = document.getElementById("right");
+
+    if (cryHover) {
+      cryHover.addEventListener("mouseenter", () => handleMouseEnter("cry"));
+      cryHover.addEventListener("mouseleave", () => handleMouseLeave("cry"));
+    }
+    if (prevHover) {
+      prevHover.addEventListener("mouseenter", () => handleMouseEnter("prev"));
+      prevHover.addEventListener("mouseleave", () => handleMouseLeave("prev"));
+    }
+    if (nextHover) {
+      nextHover.addEventListener("mouseenter", () => handleMouseEnter("next"));
+      nextHover.addEventListener("mouseleave", () => handleMouseLeave("next"));
+    }
+    if (leftHover) {
+      leftHover.addEventListener("mouseenter", () => handleMouseEnter("left"));
+      leftHover.addEventListener("mouseleave", () => handleMouseLeave("left"));
+    }
+    if (rightHover) {
+      rightHover.addEventListener("mouseenter", () => handleMouseEnter("right"));
+      rightHover.addEventListener("mouseleave", () => handleMouseLeave("right"));
+    }
+    
+
+    return () => {
+      document.body.removeEventListener("pointermove", handlePointerMove);
+      if (cryHover) {
+        cryHover.removeEventListener("mouseenter", () => handleMouseEnter("cry"));
+        cryHover.removeEventListener("mouseleave", () => handleMouseLeave("cry"));
+      }
+      if (prevHover) {
+        prevHover.removeEventListener("mouseenter", () => handleMouseEnter("prev"));
+        prevHover.removeEventListener("mouseleave", () => handleMouseLeave("prev"));
+      }
+      if (nextHover) {
+        nextHover.removeEventListener("mouseenter", () => handleMouseEnter("next"));
+        nextHover.removeEventListener("mouseleave", () => handleMouseLeave("next"));
+      }
+      if (leftHover) {
+        leftHover.removeEventListener("mouseenter", () => handleMouseEnter("left"));
+        leftHover.removeEventListener("mouseleave", () => handleMouseLeave("left"));
+      }
+      if (rightHover) {
+        rightHover.removeEventListener("mouseenter", () => handleMouseEnter("right"));
+        rightHover.removeEventListener("mouseleave", () => handleMouseLeave("right"));
+      }
+    };    
+  }, []);
 
 
   return (
@@ -190,11 +273,82 @@ const handleMouseMove = (e) => {
           </div>
 
           <div className="buttons-container">
-            <div className="black-button" onClick={PlayCry}><span className="tooltiptext">Cry</span></div>
+
+            <div id="black-button" className="black-button" onClick={PlayCry}>
+              {tooltipCryVisible && (
+                <div
+                  style={{
+                    position: "fixed",
+                    left: position.x + 40,
+                    top: position.y + 20,
+                    width: "5em",
+                    height: "1.75em",
+                    backgroundColor: "#343434",
+                    border: "2px solid black",
+                    borderRadius: "6px",
+                    pointerEvents: "none",
+                    transform: "translate(-50%, -50%)",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "white",
+                    fontsize: ".75em",
+                    zIndex: "99",
+                  }}>Cry</div>
+                )}
+            </div>
+
             <div className="color-buttons">
               <div className="small-buttons">
-                <div className="red-button" onClick={() => toggleScreen(-1)}><span className="tooltiptext">Prev</span></div>
-                <div className="blue-button" onClick={() => toggleScreen(1)}><span className="tooltiptext">Next</span></div>
+                
+                <div id="red-button" className="red-button" onClick={() => pokemonName && toggleScreen(-1)}>
+                {tooltipPrevVisible && (
+                <div
+                  style={{
+                    position: "fixed",
+                    left: position.x + 50,
+                    top: position.y + 20,
+                    width: "6em",
+                    height: "1.75em",
+                    backgroundColor: "#343434",
+                    border: "2px solid black",
+                    borderRadius: "6px",
+                    pointerEvents: "none",
+                    transform: "translate(-50%, -50%)",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "white",
+                    fontsize: ".75em",
+                    zIndex: "99",
+                  }}>Prev</div>
+                )}
+                </div>
+                
+                <div id="blue-button" className="blue-button" onClick={() => pokemonName && toggleScreen(1)}>
+                {tooltipNextVisible && (
+                <div
+                  style={{
+                    position: "fixed",
+                    left: position.x + 50,
+                    top: position.y + 20,
+                    width: "6em",
+                    height: "1.75em",
+                    backgroundColor: "#343434",
+                    border: "2px solid black",
+                    borderRadius: "6px",
+                    pointerEvents: "none",
+                    transform: "translate(-50%, -50%)",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "white",
+                    fontsize: ".75em",
+                    zIndex: "99",
+                  }}>Next</div>
+                )}
+                </div>
+
               </div>
               <div className="green-section"></div>
             </div>
@@ -205,8 +359,54 @@ const handleMouseMove = (e) => {
                 <div className='down'></div>
               </div>
               <div className='horizontal'>
-                <div className='left' onClick={() => {ButtonClick(); fetchData(pokemonNumber - 1);}}><span className="tooltiptext">Prev Pokemon</span></div>
-                <div className='right' onClick={() => {ButtonClick(); fetchData(pokemonNumber + 1);}}><span className="tooltiptext">Next Pokemon</span></div>
+                <div id="left" className='left' onClick={() => {ButtonClick(); fetchData(pokemonNumber - 1);}}>
+                {tooltipLeftVisible && (
+                <div
+                  style={{
+                    position: "fixed",
+                    left: position.x + 80,
+                    top: position.y + 20,
+                    width: "10em",
+                    height: "1.75em",
+                    backgroundColor: "#343434",
+                    border: "2px solid black",
+                    borderRadius: "6px",
+                    pointerEvents: "none",
+                    transform: "translate(-50%, -50%)",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "white",
+                    fontsize: ".75em",
+                    zIndex: "99",
+                    padding: "5px 0",
+                  }}>Prev Pokemon</div>
+                )}
+                </div>
+                <div id="right" className='right' onClick={() => {ButtonClick(); fetchData(pokemonNumber + 1);}}>
+                {tooltipRightVisible && (
+                <div
+                  style={{
+                    position: "fixed",
+                    left: position.x + 80,
+                    top: position.y + 20,
+                    width: "10em",
+                    height: "1.75em",
+                    backgroundColor: "#343434",
+                    border: "2px solid black",
+                    borderRadius: "6px",
+                    pointerEvents: "none",
+                    transform: "translate(-50%, -50%)",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "white",
+                    fontsize: ".75em",
+                    zIndex: "99",
+                    padding: "5px 0",
+                  }}>Next Pokemon</div>
+                )}
+                </div>
               </div>
             </div>
           </div>
